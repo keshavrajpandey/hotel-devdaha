25d0d86e33e91144cca91428270ff7c7a1cf6563
Keshav <keshavrajpandey81@gmail.com>
2025-11-22 11:55:07 +0545

    Disable lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -25,9 +25,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <ColorSchemeScript />
-      </head>
       <body className={`${inter.className} bg-white text-gray-800`}>
         <MantineProvider>
           <Navbar />
